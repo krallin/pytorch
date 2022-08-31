5aad04351a3b7f68003ad77f9698e08509b6fd19
soulitzer <soulitzer@gmail.com>
2022-08-31 15:17:52 -0400

    More decompositions for jvp from functorch into core
    ghstack-source-id: 7f6e82e4c02df1503cd5f6d30baa596980378865
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/84358

--- functorch/functorch/_src/aot_autograd.py
+++ functorch/functorch/_src/aot_autograd.py
@@ -11,6 +11,7 @@ import torch.nn as nn
 import torch.utils._pytree as pytree
 import torch.utils.dlpack
 from torch import Tensor
+from torch._decomp import register_decomposition
 from torch._subclasses import FakeTensorMode
 from torch.fx import immutable_collections, Interpreter
 from torch.nn.utils import stateless
@@ -19,7 +20,6 @@ from functorch import make_fx
 from functorch._C import CompileCache
 from functorch.experimental import functionalize
 from . import config
-from .decompositions import register_decomposition
 from .named_members_polyfill import _named_buffers, _named_parameters
 from .partitioners import default_partition
 
